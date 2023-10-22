6101f6789e159c61f320cb2afe385098ccfc9b87
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-22 08:48:01 -0700

    initial commit

---
+++ contract/contracts/RSKNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract RSKNFT is ERC721URIStorage{
+    
+}
