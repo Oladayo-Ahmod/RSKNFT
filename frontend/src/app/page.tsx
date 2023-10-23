ab057e08482d5b8b42f8729557c2b99ae3a59057
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-23 14:48:13 -0700

    content component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,14 @@ import Image from 'next/image'
 // import styles from './page.module.css'
 // import styles from './styles.module.css'
 import Header from './components/Header'
+import Content from './components/Content'
 
 export default function Home() {
   return (
       <main>
         <Header />
+        <Content />
+        
       </main>
   )
 }
