3bf5b13b64a947ddcf01f1971a3d9a5f49330cf5
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-24 09:17:09 -0700

    wallets component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 // import styles from './styles.module.css'
 import Header from './components/Header'
 import Slider from './components/Slider'
+import Wallets from './components/Wallets'
 
 export default function Home() {
   return (
       <main>
         <Header />
         <Slider />
+        <Wallets />
         
       </main>
   )
