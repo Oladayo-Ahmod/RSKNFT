98c71efc5220175899eecbe09ff61ccee4100838
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-25 08:32:31 -0700

    trending nfts component

--- frontend/src/app/components/TrendingNFT.tsx
+++ frontend/src/app/components/TrendingNFT.tsx
@@ -1,5 +1,10 @@
 "use client"
 import Image from 'next/image'
+import { Swiper } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/bundle'
+import { SwiperSlide } from 'swiper/react'
+import { Navigation,Scrollbar,Autoplay } from 'swiper/modules'
 
 function TrendingNFT(){
     return (
@@ -33,52 +38,65 @@ function TrendingNFT(){
                     <div className="row">
                         <div className="col-lg-12">
                             <h2>Trending NFTs</h2>
+                            <Swiper 
+                                modules={[Navigation, Scrollbar,Autoplay]}
+                                navigation
+                                slidesPerView={4}
+                                autoplay={{delay: 3000}}
+                                onSlideChange={(swiper)=> console.log('swiper change')}
+                                onSwiper={(swiper)=> console.log('swiper set')}
+                            >
+
                             <div id="items-carousel-s2" className="owl-carousel wow fadeIn" style={{'visibility':'visible'}}>
                                 {/* nft item begin */}
-                                <div className="d-item">
-                                    <div className="nft__item s2">
-                                        {/* <div className="de_countdown" data-year="2023" data-month="11" data-day="8" data-hour="8"></div> */}
-                                        <div className="author_list_pp">
-                                            <a href="grey-author.html" data-bs-toggle="tooltip" data-bs-placement="top" title="Creator: Gigaland">                                    
-                                                <Image className="lazy" src="/author/author-4.jpg" width={50} height={50} alt="" />
-                                                <i className="fa fa-check"></i>
-                                            </a>
-                                        </div>
-                                        <div className="nft__item_wrap">
-                                            <div className="nft__item_extra">
-                                                <div className="nft__item_buttons">
-                                                    <button data-bs-toggle="modal" data-bs-target="#buy_now">Buy Now</button>
-                                                    <button data-bs-toggle="modal" data-bs-target="#place_a_bid">Place a Bid</button>
-                                                    <div className="nft__item_share">
-                                                        <h4>Share</h4>
-                                                        <a href="https://www.facebook.com/sharer/sharer.php?u=https://madebydesignesia.com/themes" target="_blank"><i className="fa fa-facebook fa-lg"></i></a>
-                                                        <a href="https://twitter.com/intent/tweet?url=https://madebydesignesia.com/themes" target="_blank"><i className="fa fa-twitter fa-lg"></i></a>
-                                                        <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://madebydesignesia.com/themes"><i className="fa fa-envelope fa-lg"></i></a>
+                                <SwiperSlide>
+                                    <div className="d-item">
+                                        <div className="nft__item s2">
+                                            {/* <div className="de_countdown" data-year="2023" data-month="11" data-day="8" data-hour="8"></div> */}
+                                            <div className="author_list_pp">
+                                                <a href="grey-author.html" data-bs-toggle="tooltip" data-bs-placement="top" title="Creator: Gigaland">                                    
+                                                    <Image className="lazy" src="/author/author-4.jpg" width={50} height={50} alt="" />
+                                                    <i className="fa fa-check"></i>
+                                                </a>
+                                            </div>
+                                            <div className="nft__item_wrap">
+                                                <div className="nft__item_extra">
+                                                    <div className="nft__item_buttons">
+                                                        <button data-bs-toggle="modal" data-bs-target="#buy_now">Buy Now</button>
+                                                        <button data-bs-toggle="modal" data-bs-target="#place_a_bid">Place a Bid</button>
+                                                        <div className="nft__item_share">
+                                                            <h4>Share</h4>
+                                                            <a href="https://www.facebook.com/sharer/sharer.php?u=https://madebydesignesia.com/themes" target="_blank"><i className="fa fa-facebook fa-lg"></i></a>
+                                                            <a href="https://twitter.com/intent/tweet?url=https://madebydesignesia.com/themes" target="_blank"><i className="fa fa-twitter fa-lg"></i></a>
+                                                            <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://madebydesignesia.com/themes"><i className="fa fa-envelope fa-lg"></i></a>
+                                                        </div>
                                                     </div>
                                                 </div>
+                                                <a href="07_modern-item-details.html">
+                                                    <div className="d-placeholder"></div>
+                                                    <Image src="/items/anim-10.webp" className="lazy nft__item_preview" width={224} height={224} alt="" />
+                                                </a>
                                             </div>
-                                            <a href="07_modern-item-details.html">
-                                                <div className="d-placeholder"></div>
-                                                <Image src="/items/anim-10.webp" className="lazy nft__item_preview" width={224} height={224} alt="" />
-                                            </a>
-                                        </div>
-                                        <div className="nft__item_info">
-                                            <a href="07_modern-item-details.html">
-                                                <h4>Defraudment</h4>
-                                            </a>
-                                            <div className="nft__item_click">
-                                                <span></span>
-                                            </div>
-                                            <div className="nft__item_price">
-                                                0.09 ETH<span>1/14</span>
-                                            </div>
-                                            <div className="nft__item_like">
-                                                <i className="fa fa-heart"></i><span>94</span>
+                                            <div className="nft__item_info">
+                                                <a href="07_modern-item-details.html">
+                                                    <h4>Defraudment</h4>
+                                                </a>
+                                                <div className="nft__item_click">
+                                                    <span></span>
+                                                </div>
+                                                <div className="nft__item_price">
+                                                    0.09 ETH<span>1/14</span>
+                                                </div>
+                                                <div className="nft__item_like">
+                                                    <i className="fa fa-heart"></i><span>94</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                {/* nft item begin */}
+                                </SwiperSlide>
+                                
+                                <SwiperSlide>
+                                    {/* nft item begin */}
                                 <div className="d-item">
                                     <div className="nft__item s2">
                                         <div className="author_list_pp">
@@ -121,7 +139,10 @@ function TrendingNFT(){
                                         </div>
                                     </div>
                                 </div>
-                                {/* nft item begin */}
+                                </SwiperSlide>
+                                
+                                <SwiperSlide>
+                                    {/* nft item begin */}
                                 <div className="d-item">
                                     <div className="nft__item s2">
                                         <div className="de_countdown" data-year="2023" data-month="11" data-day="12" data-hour="8"></div>
@@ -165,7 +186,11 @@ function TrendingNFT(){
                                         </div>
                                     </div>
                                 </div>
-                                {/* nft item begin */}
+                                </SwiperSlide>
+                                
+
+                                <SwiperSlide>
+                                    {/* nft item begin */}
                                 <div className="d-item">
                                     <div className="nft__item s2">
                                         <div className="author_list_pp">
@@ -208,6 +233,7 @@ function TrendingNFT(){
                                         </div>
                                     </div>
                                 </div>
+                                </SwiperSlide>
                                 {/* nft item begin */}
                                 <div className="d-item">
                                     <div className="nft__item s2">
@@ -296,6 +322,7 @@ function TrendingNFT(){
                                     </div>
                                 </div>
                             </div>
+                            </Swiper>
                         </div>
 
 
