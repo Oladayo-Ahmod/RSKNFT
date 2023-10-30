89dd3a2423a6e4e033c547e53dfea1099c699c73
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-30 09:58:37 -0700

    create page functionality

---
+++ frontend/src/app/components/CreatePageSubHeader.tsx
@@ -0,0 +1,25 @@
+function CreatePageSubHeader(){
+    return (
+        <div className="no-bottom no-top" id="content">
+            <div id="top"></div>
+
+            <section id="subheader" className="text-light" style={{'background' : 'url(/background/subheader.jpg) top'}}>
+                <div className="center-y relative text-center">
+                    <div className="container">
+                        <div className="row">
+
+                            <div className="col-md-12 text-center">
+                                <h1>Create Single Collectible</h1>
+                            </div>
+                            <div className="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+           
+    )
+}
+
+export default CreatePageSubHeader
--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -1,21 +1,17 @@
 import Image from 'next/image'
 // import styles from './page.module.css'
 // import styles from './styles.module.css'
-import Header from '../components/Header'
-import Slider from '../components/Slider'
-import Wallets from '../components/Wallets'
-import TopSeller from '../components/TopSellers'
-import TrendingNFT from '../components/TrendingNFT'
+import CreatePageHeader from '../components/CreatePageHeader'
+import CreatePageSubHeader from '../components/CreatePageSubHeader'
 import Footer from '../components/Footer'
+import CreatePageContent from '../components/CreatePageContent'
 
 export default function Create() {
   return (
-      <main style={{'position': 'absolute'}}>
-        <Header />
-        <Slider />
-        <Wallets />
-        <TopSeller />
-        <TrendingNFT />
+      <main >
+        <CreatePageHeader />
+        <CreatePageSubHeader />
+        <CreatePageContent />
         <Footer />
         
       </main>
